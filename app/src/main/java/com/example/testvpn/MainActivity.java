5e5f8534a29df5b9fa6335ac31063c1b85a18cc5
Ikramul Murad <ikramul.murad1993@gmail.com>
2020-10-04 11:33:14 +0600

    fix logical error

--- app/src/main/java/com/example/testvpn/MainActivity.java
+++ app/src/main/java/com/example/testvpn/MainActivity.java
@@ -2,29 +2,36 @@ package com.example.testvpn;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private boolean isConnected = false;
-    final Button connectionButton = findViewById(R.id.connectionButton);
+    private Context context;
+    private boolean isConnected;
+    private Button connectionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        context = this;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        isConnected = false;
+        connectionButton = findViewById(R.id.connectionButton);
         connectionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (isConnected) {
-                    Toast.makeText(getApplicationContext(), "Connected to the VPN", Toast.LENGTH_LONG).show();
-                    connectionButton.setText("Disconnect");
-                } else {
-                    Toast.makeText(getApplicationContext(), "Disconnected from the VPN", Toast.LENGTH_LONG).show();
+                    isConnected = false;
+                    Toast.makeText(context, "Disconnected from the VPN", Toast.LENGTH_SHORT).show();
                     connectionButton.setText("Connect");
+                } else {
+                    isConnected = true;
+                    Toast.makeText(context, "Connected to the VPN", Toast.LENGTH_SHORT).show();
+                    connectionButton.setText("Disconnect");
                 }
             }
         });
